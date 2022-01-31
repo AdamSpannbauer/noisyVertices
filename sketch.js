d9eecec2c7d2d948ee1f7420e58244e90b486d77
Adam Spannbauer <SpannbauerAdam@gmail.com>
2022-01-31 15:07:10 -0500

    add checkbox for vertex type

--- sketch.js
+++ sketch.js
@@ -3,6 +3,13 @@ import { genNoisyShape } from './genNoisyShape.js';
 let cnv;
 let genBtn;
 let saveBtn;
+let curveCheckbox;
+
+let curveVertices;
+
+function curveCheckEvent() {
+  curveVertices = this.checked();
+}
 
 const genShape = () => {
   background(255);
@@ -11,7 +18,6 @@ const genShape = () => {
   const nVertices = 8; // each vertex is a bend in the line
   const nSteps = 32; // each step is a new line
   const noiseStep = 0.01; // how far can the vertices move between each new line
-  const curveVertices = true; // should the lines be curvy or pointy
 
   genNoisyShape(nVertices, nSteps, noiseStep, curveVertices);
 };
@@ -35,9 +41,15 @@ window.setup = () => {
   saveBtn.position(0, 25);
   saveBtn.mousePressed(saveShape);
 
+  curveCheckbox = createCheckbox('Curved vertices', true);
+  curveCheckbox.changed(curveCheckEvent);
+  curveCheckbox.position(0, 50);
+
   noFill();
   stroke(0);
   strokeWeight(2);
+
+  genShape();
 };
 
 window.draw = () => {
